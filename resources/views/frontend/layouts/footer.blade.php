e43344e7dacffb899682519e1b64a08ffed62ebb
usman-shoukat <usmangee862@gmail.com>
2021-04-12 11:16:26 -0700

    complete

--- resources/views/frontend/layouts/footer.blade.php
+++ resources/views/frontend/layouts/footer.blade.php
@@ -74,7 +74,8 @@
 					<div class="row">
 						<div class="col-lg-6 col-12">
 							<div class="left">
-								<p>Copyright © {{date('Y')}} <a href="https://github.com/Prajwal100" target="_blank">Prajwal Rai</a>  -  All Rights Reserved.</p>
+								<p>Copyright © {{date('Y')}} <a href="https://github.com/usman-shoukat" target="_blank">usman shoukat</a>  -  All Rights Reserved.</p>
+								
 							</div>
 						</div>
 						<div class="col-lg-6 col-12">
